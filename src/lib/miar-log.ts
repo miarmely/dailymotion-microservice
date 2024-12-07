b1bd869e73495c5232a6849fd871e4b192908d7e
miarmely <comp.eng.m.akdemir@gmail.com>
2024-12-07 17:30:17 +0300

    (*) Some bugs in 'miar-log' library is fixed.

--- src/lib/miar-log.ts
+++ src/lib/miar-log.ts
@@ -8,8 +8,17 @@ class MiarLog {
     /* with data
     */ log(alertType: AlertTypes, log: string, data: { [k: string]: any }): void
     log(alertType: AlertTypes, log: string, data?: any) {
-        console.log(`${this.getNowDateInISO()} - ${alertType} - ${log}`);
-        if (data) console.log(data, "\n")
+        { // write log message
+            console.log(`${this.getNowDateInISO()} - ${alertType} - ${log}`);
+        }
+        { // write data if wants
+            // convert data in string to object
+            const dataInObj = typeof data === "string" ?
+                { info: data }
+                : data as { [k: string]: any }
+
+            if (data) console.log(dataInObj, "\n")
+        }
     }
 }
 
